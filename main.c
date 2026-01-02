449090d051d5ea30252a8c1cbe71f24cbbba1ff5
hourin <phucjaki@gmail.com>
2026-01-02 22:34:29 +0700

    Added: rendering a line

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
-#include <stdio.h>
+#include "hdoc.h"
+#include <raylib.h>
 
 int main(void)
 {
+        InitWindow(PAGE_WIDTH, PAGE_HEIGHT, "HDocViewer");
+
+        Image canvas = new_page();
+        struct hdoc_State state = default_state();
+        render_line(&state, &canvas, "test");
+
+        Texture render = LoadTextureFromImage(canvas);
+        UnloadImage(canvas);
+
+        while (!WindowShouldClose()) {
+                BeginDrawing();
+                DrawTexture(render, 0, 0, WHITE);
+                EndDrawing();
+        }
+
+        UnloadTexture(render);
+        CloseWindow();
         return 0;
 }
 
