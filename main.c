c134fbc10962494c281be3baaea7f2fd8e634bb3
hourin <phucjaki@gmail.com>
2026-01-03 22:45:10 +0700

    Added: main() now reads file given with cli args

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
-#include "hdoc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <raylib.h>
 
-int main(void)
+#include "hdoc.h"
+
+int main(int argc, char **argv)
 {
         InitWindow(PAGE_WIDTH, PAGE_HEIGHT, "HDocViewer");
 
         Image canvas = new_page();
         struct hdoc_State state = default_state();
-        render(&state, &canvas, "test $$r hehe $$b test test balls $$y   chowayo");
+
+        char help[BUFSIZ];
+        sprintf(help, "HDocViewer. Use $$r %s $$b <file>\
+                        $$d to render file.", argv[0]);
+
+        if (argc > 1) {
+                char *buf = chomp_file(argv[1]);
+                render(&state, &canvas, buf);
+                free(buf);
+        } else
+                render(&state, &canvas, help);
 
         Texture render = LoadTextureFromImage(canvas);
         UnloadImage(canvas);
